bda59d654e282b67a9c3cc5c7a1fe14d98fe05ea
IARMC <ricardomaquilonchacha@gmail.com>
2020-10-25 23:37:25 -0500

    v0.2
    Preprocesamiento

--- Kalman filter.R
+++ Kalman filter.R
@@ -17,3 +17,17 @@ con <- dbConnect(drv, host = host, port = port, dbname = dbname, user = user, pa
 dataset <- "brasil"
 data <- dbGetQuery(con, "SELECT longitude, latitude, unixtime, file_name FROM public.brasil WHERE  longitude BETWEEN  '-37.095' and '-37.04' and latitude BETWEEN  '-10.99' and '-10.89' and unixtime > '13/09/14' and unixtime <= '29/07/15' ")
 dbDisconnect(con)
+
+#####     Preprocesamiento     #####
+sapply(dataset, function(x) sum(is.na(x)))
+dataset <- dataset[!is.na(dataset$longitude),]
+dataset <- dataset[!is.na(dataset$latitude),]
+dataset <- dataset[!is.na(dataset$file_name),]
+dataset <- dataset[!is.na(dataset$unixtime),]
+
+vk <- c()
+vlat <- c()
+vlon <- c()
+
+#####     Division de los datos por trayectorias     #####
+array_name <- data.frame(file_name=unique(data$file_name)) 
