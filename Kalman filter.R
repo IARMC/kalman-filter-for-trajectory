adcdd2c1ade5b3717c4b762b4027e6848a6bf41d
IARMC <ricardomaquilonchacha@gmail.com>
2020-10-25 23:21:06 -0500

    v0.1
    Configuracion inicial y Obtencion de los datos

---
+++ Kalman filter.R
@@ -0,0 +1,19 @@
+#####     Configuracion Inicial     #####
+rm(list = ls())
+options( digits = 15 )
+
+#####     Conexion a la base de datos     #####
+library(RPostgreSQL)
+dbdriver <- "PostgreSQL";
+host <- 'localhost';
+port <- '5432';
+dbname <- 'CPPP';
+user <- 'postgres';
+pass <- 'toor';
+drv <- dbDriver(dbdriver)
+con <- dbConnect(drv, host = host, port = port, dbname = dbname, user = user, pass = pass)
+
+#####     Obtencion de la informacion desde la base de datos     #####
+dataset <- "brasil"
+data <- dbGetQuery(con, "SELECT longitude, latitude, unixtime, file_name FROM public.brasil WHERE  longitude BETWEEN  '-37.095' and '-37.04' and latitude BETWEEN  '-10.99' and '-10.89' and unixtime > '13/09/14' and unixtime <= '29/07/15' ")
+dbDisconnect(con)
