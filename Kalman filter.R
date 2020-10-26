d35e61e197bff061da0a44fd9864048578866d0d
IARMC <ricardomaquilonchacha@gmail.com>
2020-10-26 00:18:44 -0500

    v0.3
    Funciones y procesamiento individual

--- Kalman filter.R
+++ Kalman filter.R
@@ -19,15 +19,58 @@ data <- dbGetQuery(con, "SELECT longitude, latitude, unixtime, file_name FROM pu
 dbDisconnect(con)
 
 #####     Preprocesamiento     #####
-sapply(dataset, function(x) sum(is.na(x)))
-dataset <- dataset[!is.na(dataset$longitude),]
-dataset <- dataset[!is.na(dataset$latitude),]
-dataset <- dataset[!is.na(dataset$file_name),]
-dataset <- dataset[!is.na(dataset$unixtime),]
+sapply(data, function(x) sum(is.na(x)))
+data <- data[!is.na(data$longitude),]
+data <- data[!is.na(data$latitude),]
+data <- data[!is.na(data$file_name),]
+data <- data[!is.na(data$unixtime),]
 
+trajectory_after_kalman <- list()
 vk <- c()
 vlat <- c()
 vlon <- c()
 
 #####     Division de los datos por trayectorias     #####
 array_name <- data.frame(file_name=unique(data$file_name)) 
+
+todas <- data.frame(longitude = c(1:nrow(data)), latitude = 0, file_name = 0, speed = 0, unixtime = 0)
+todas$longitude <- data$longitude
+todas$latitude  <- data$latitude
+todas$file_name <- data$file_name
+todas$unixtime  <- data$unixtime
+todas$speed     <- data$speed
+
+lista_trayectorias_sinprocesar <- list ()
+
+for(count in 1:length(unique(todas$file_name))){
+  tray_corresp <- subset(todas, todas$file_name == array_name$file_name[count])
+  lista_trayectorias_sinprocesar[[count]] <-  tray_corresp
+}
+
+#####     Funciones Kalman     #####
+prueba_funcion <- function(pkg) {
+  #ambito local de la funcion
+  valor <- 5
+  print(valor)
+}
+
+
+
+
+#####     Recorrido de trayectorias     #####
+cant_tr <- length(unique(lista_trayectorias_sinprocesar))
+
+for (count in 1:cant_tr){
+  trayectoria_individual <- lista_trayectorias_sinprocesar[[count]]
+  cant_points <- nrow(trayectoria_individual)
+  
+  if(cant_points <= 2){
+    
+    
+  }else{
+    
+    
+  }
+}
+
+
