82ce7e284970b51c1cc9b94f755eb5773f8068ec
IARMC <ricardomaquilonchacha@gmail.com>
2020-11-04 23:14:35 -0500

    v0.15
    Pruebas iniciales con libreria

--- Kalman filter.R
+++ Kalman filter.R
@@ -241,8 +241,85 @@ save_trajectories(trajectory_after_kalman)
 
 
 
+#####     Comprobacion con libreria     #####
+#dlm
+library(dlm)
 
-#####     Graficas en Mapa     #####
+kalman_libreria <- list()
+
+nileBuild <- function(par) {
+  dlmModPoly(order = 1, dV = exp(par[1]), dW = exp(par[2]))
+}
+
+cant_tr <- length(unique(lista_trayectorias_sinprocesar))
+
+for (count in 1:cant_tr){
+  #count <- 93
+  print(paste("Aplicando DLM Kalman. Trayectoria ",count,"...", sep = ""))
+  tr_i <- lista_trayectorias_sinprocesar[[count]]
+  cant_points <- nrow(tr_i)
+  frame_tray <- data.frame(x = lista_trayectorias_sinprocesar[[count]]$longitude, 
+                           y = lista_trayectorias_sinprocesar[[count]]$latitude, 
+                           t = lista_trayectorias_sinprocesar[[count]]$unixtime,
+                           g = 0)
+  if(cant_points <= 6){
+    new_frame_kalman <- frame_tray
+    print("Menos de 6")
+  }else{
+    matrix1 <- frame_tray
+    
+    nileMLE1 <- dlmMLE(matrix1[,1],parm = c(1, 1),nileBuild)
+    nileMLE2 <- dlmMLE(matrix1[,2],parm = c(1, 1),nileBuild)
+    
+    nileMod1 <- nileBuild(nileMLE1$par)
+    nileMod2 <- nileBuild(nileMLE2$par)
+    
+    nileFilt1 <- dlmFilter(matrix1[,1], nileMod1)
+    nileFilt2 <- dlmFilter(matrix1[,2], nileMod2)
+    
+    nileSmooth1 <- dlmSmooth(nileFilt1)
+    nileSmooth2 <- dlmSmooth(nileFilt2)
+    
+    tamano_kalman <- length(nileSmooth1$s)
+    new_frame_kalman <- data.frame(x = nileSmooth1$s[2:tamano_kalman], 
+                                   y = nileSmooth2$s[2:(tamano_kalman)], 
+                                   t = frame_tray[,3], 
+                                   g = frame_tray[,4])
+  }
+  
+  kalman_libreria[[count]] <- new_frame_kalman
+}
+
+
+sel_tr <- 5
+kalman_libreria[[5]]
+guardar_trayectoria_individual(sel_tr, lista_trayectorias_sinprocesar[[sel_tr]], trajectory_after_kalman[[sel_tr]])
+nombre <- paste("Comparativa trayectoria ",sel_tr,".png",sep = "")
+png(filename = nombre, width = 1024, height = 720, units = 'px', pointsize = 20 ,res = NA)
+plot(x = lista_trayectorias_sinprocesar[[sel_tr]]$longitude, y=lista_trayectorias_sinprocesar[[sel_tr]]$latitude,type="l",col="red", xlab="Longitud", ylab="Latitud", main=paste("Results Kalman Filter_TR",sel_tr,sep = ""))
+points(x = trajectory_after_kalman[[sel_tr]]$longitude, y=trajectory_after_kalman[[sel_tr]]$latitude,type="l",col="blue")
+points(x = kalman_libreria[[sel_tr]]$x, y=kalman_libreria[[sel_tr]]$y,type="l",col="green")
+#https://www.rdocumentation.org/packages/graphics/versions/3.6.2/topics/legend
+legend("bottomleft",legend=c("Original","Filtro de Kalman","Libreria"),col=c("red","blue","green"), pch=1,bty="n",ncol=1,cex=1,pt.cex=1)
+#Aplicar zoom al grafico
+#zoom::zm()
+dev.off()
+
+idtr <- 5
+nombre <- paste("trayectoria_",idtr,".png",sep = "")
+png(filename=nombre, width = 1024, height = 720, units = 'px', pointsize = 20 ,res = NA)
+plot(x=tr_original$longitude, y=tr_original$latitude,type="l",col="red", xlab="Longitud", ylab="Latitud", main=paste("Results Kalman Filter_TR",idtr,sep = ""))
+points(x=tr_kalman$longitude, y=tr_kalman$latitude,type="l",col="blue")
+#https://www.rdocumentation.org/packages/graphics/versions/3.6.2/topics/legend
+legend("bottomleft",legend=c("Original","Filtro de Kalman"),col=c("red","blue"), pch=1,bty="n",ncol=1,cex=1,pt.cex=1)
+#Aplicar zoom al grafico
+#zoom::zm()
+dev.off()
+
+
+#####     Graficacion en Mapa     #####
+packages <- c("RPostgreSQL","rlang","kmlShape","brotli","dlm", "ggplot2", "caret", "class", "mapview",  "compare", "pracma" , "stringr","SpatialTools","matlib", "dplyr","chron","lubridate","zoom","RgoogleMaps","ggmap") #librerias 
+
 #install.packages("leaflet")
 #install.packages("mapview")
 packages <- c("RPostgreSQL","rlang", "ggplot2", "caret", "class", "mapview",  "compare", "pracma", "leaflet" , "stringr","SpatialTools","matlib", "dplyr","chron","lubridate","zoom","RgoogleMaps","ggmap") #librerias
@@ -256,7 +333,7 @@ ipak(packages)
 
 m <- leaflet()
 m <- addTiles(m)
-n_tr <- 88
+n_tr <- 42
 m <- addCircles(map = m, lng=lista_trayectorias_sinprocesar[[n_tr]]$longitude, lat =  lista_trayectorias_sinprocesar[[n_tr]]$latitude, radius = 0.05, weight = 5, color="red")
 m <- addCircles(map = m, lng=trajectory_after_kalman[[n_tr]]$longitude, lat =  trajectory_after_kalman[[n_tr]]$latitude, radius = 0.05, weight = 5, color="blue")
 #m <- addCircles(map = m, lng=trajectory_after_kalman[[n_tr]]$longitude, lat =  trajectory_after_kalman[[n_tr]]$latitude, radius = 0.05, weight = 5, color="yellow")
@@ -270,6 +347,7 @@ m
 #https://www.rdocumentation.org/packages/webshot/versions/0.5.2/topics/install_phantomjs
 install_phantomjs(version = "2.1.1",
                   baseURL = "https://github.com/wch/webshot/releases/download/v0.3.1/")
+#webshot::install_phantomjs()
 getwd()
 #mapshot(m, url = "map.html", file = "Mapa localidad y trayectorias.png")
 mapshot(m, url = paste0(getwd(), "/map.html"), file = paste0(getwd(), "/map.png"))
