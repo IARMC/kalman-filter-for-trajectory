069fc63fe5d02b26bcd9a30f0524364f904c475e
IARMC <ricardomaquilonchacha@gmail.com>
2020-12-15 09:39:53 -0500

    v0.22.1
    Estructura de las secciones

--- Replicas/DRP Kalman Dev.R
+++ Replicas/DRP Kalman Dev.R
@@ -2,3 +2,22 @@
 rm(list = ls())
 options( digits = 15 )
 
+#####     Funciones Generales     #####
+#Importacion de librerias
+ipak <- function(pkg) { #cargar e instalar paquetes de manera dinamica
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+#####     Funciones de Tratamiento de Bitacoras del Filtro de Kalman     #####
+
+
+#####     Funciones de Tratamiento del Filtro de Kalman     #####
+
+
+#####     Funciones de Almacenamiento de Resultados del Filtro de Kalman     #####
+
+
+#####     Funciones de Tratamiento de Algoritmos de Simplificacion     #####
